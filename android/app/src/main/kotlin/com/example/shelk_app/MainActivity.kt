4675c7edb01a898a6942697231069c36ab333c41
fedunoff <114008375+fedunoff@users.noreply.github.com>
2022-10-05 04:54:00 +1100

    git remote add origin https://github.com/fedunoff/shelk.git

---
+++ android/app/src/main/kotlin/com/example/shelk_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shelk_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(FlutterTutorialApp());
+
+class FlutterTutorialApp extends StatelessWidget {
+  const FlutterTutorialApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "fluter tut",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Фотостудия Шелк"),
+          centerTitle: true,
+          backgroundColor: Colors.purple,
+        ),
+        body: RichText(
+          text: const TextSpan(
+            style: TextStyle(
+              fontSize: 20.0,
+              fontStyle: FontStyle.italic,
+              color: Colors.black,
+              fontFamily: "loraR",
+            ),
+            children: <TextSpan>[
+              TextSpan(text: "Hello, "),
+              TextSpan(
+                style: TextStyle(color: Colors.red),
+                children: <TextSpan>[
+                  TextSpan(text: "Brave "),
+                  TextSpan(text: "new "),
+                  TextSpan(
+                    text: "world",
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ],
+              ),
+              TextSpan(text: '!'),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text("тык"),
+          onPressed: () {},
+          backgroundColor: Colors.purple,
+        ),
+      ),
+    );
+  }
+}
+//
